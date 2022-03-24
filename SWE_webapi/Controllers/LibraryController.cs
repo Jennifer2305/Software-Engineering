74352d824180a1fa663cc32146982b3c87596633
JudithJanse <59368871+JudithJanse@users.noreply.github.com>
2022-03-24 14:38:26 +0100

    Update LibraryController.cs

--- SWE_webapi/Controllers/LibraryController.cs
+++ SWE_webapi/Controllers/LibraryController.cs
@@ -4,31 +4,33 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SWE_webapi.Models;
 
-namespace PrototypeApi.Controllers
-{
-    /*[Route("api/[controller]")]
-    public class UsersController : Controller
+
+namespace PrototypeApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : Controller
     {
-        private readonly LibraryContext _context;
+    private readonly LibraryContext _context;
 
-        public UsersController(LibraryContext context)
+    public UsersController(LibraryContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet(Name = "GetBooks")]
+    public async Task<IActionResult> Get()
+    {
+        var users = await _context.Authors
+            .Include(u => u.Books)
+            .ToArrayAsync();
+
+        var response = users.Select(u => new
         {
-            _context = context;
-        }
+            Name = u.Name,
+            posts = u.Books.Select(p => p.Title)
+        });
 
-        public async Task<IActionResult> Get()
-        {
-            var users = await _context.Authors
-                .Include(u => u.Books)
-                .ToArrayAsync();
-
-            var response = users.Select(u => new
-            {
-                Name = u.Name,
-                posts = u.Books.Select(p => p.Title)
-            });
-
-            return Ok(response);
-        }
-    }*/
+        return Ok(response);
+    }
 }
